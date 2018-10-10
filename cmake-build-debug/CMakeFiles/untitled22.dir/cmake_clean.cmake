17fdeb0e4c627ec139b1231511f77fc3e7fb60c3
killer <linhnkth1908007@fpt.edu.vn>
2018-10-10 20:07:18 +0700

    bt

---
+++ cmake-build-debug/CMakeFiles/untitled22.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled22.dir/main.c.obj"
+  "untitled22.pdb"
+  "untitled22.exe"
+  "untitled22.exe.manifest"
+  "libuntitled22.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled22.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
